45fdd7cccf0c68c83d8691a95b161c5005acb163
otisdev95 <olaliotis95@gmail.com>
2023-11-10 18:38:10 +0100

    boiler-mongomonoose

--- myApp.js
+++ myApp.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { 
 useNewUrlParser: true, useUnifiedTopology: true });
 
-const mySecret = process.env['MONGO_URI']
-
 console.log("Hello World");
 
 let Person;
