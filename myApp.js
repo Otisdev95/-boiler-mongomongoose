a71c21d60473ba8119789631bb06e4f4c5785dda
otisdev95 <olaliotis95@gmail.com>
2023-11-12 14:48:08 +0100

    boiler-mongomonoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
+
+const dotenv = require('dotenv');
+dotenv.config({ path: 'sample.env'});
+
 const mongoose = require('mongoose');
+
 mongoose.connect(process.env.MONGO_URI, { 
 useNewUrlParser: true, useUnifiedTopology: true });
 
